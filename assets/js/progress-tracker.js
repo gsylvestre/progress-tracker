5e4dda746b88a0a299d3385a57e293de2ebac0f2
Guillaume <gsylvestre@gmail.com>
2020-09-02 17:30:34 +0200

    limit bar + new bg

--- assets/js/progress-tracker.js
+++ assets/js/progress-tracker.js
@@ -4,14 +4,35 @@ const trackerContentEl = document.getElementById("tracker-content");
 const tracker = {
     init: () => {
         tracker.updateTasksList();
+
+        window.addEventListener('resize', tracker.updateLimitBar);
     },
 
     updateTasksList: () => {
         axios.get('/api/tasks').then(response => {
             trackerContentEl.innerHTML = response.data;
             tracker.listenForClick();
+            tracker.updateLimitBar();
             window.setTimeout(tracker.updateTasksList, 30000);
         });
+    },
+
+    updateLimitBar: () => {
+        let allUserLineEl = document.querySelectorAll('.user-line');
+        let minCompletedTaskId = null;
+        let minCompletedElement = null;
+        allUserLineEl.forEach(userLine => {
+             let allCompleted = userLine.querySelectorAll('.is-complete');
+             let lastCompleted = allCompleted[allCompleted.length-1];
+             let lastCompletedTaskId = parseInt(lastCompleted.dataset.taskId);
+             if (lastCompletedTaskId < minCompletedTaskId || minCompletedTaskId === null){
+                 minCompletedTaskId = lastCompletedTaskId;
+                 minCompletedElement = lastCompleted;
+             }
+        });
+        console.dir(minCompletedElement);
+        console.log(minCompletedTaskId);
+        document.getElementById("limit-bar").style.left = minCompletedElement.offsetLeft + minCompletedElement.offsetWidth + "px";
     },
 
     listenForClick: () => {
